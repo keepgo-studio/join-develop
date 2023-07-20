01f6eb7b73f398e7e8050c60ce2f7f5556e43cc2
keepgo-studio <keepgo.studio@gmail.com>
2023-07-21 00:00:18 +0900

    set youtube api auth flow

--- public/app.js
+++ public/app.js
@@ -1,64 +1,57 @@
+const API_KEY = "AIzaSyCe4wTxCLdm581_wobRgxyY60XD6jMshBg"
+
+
 function firebaseAppInit() {
-    return firebase.initializeApp({
-        apiKey: "AIzaSyCe4wTxCLdm581_wobRgxyY60XD6jMshBg",
-        authDomain: "fir-bb3d4.firebaseapp.com",
-        projectId: "fir-bb3d4",
-        storageBucket: "fir-bb3d4.appspot.com",
-        messagingSenderId: "762083454108",
-        appId: "1:762083454108:web:793ce977584f6c11115159",
-        measurementId: "G-TGB5CNB1FK"
-    })
-}
+    let isValid;
 
-async function firebaseSdkCheck() {
-    return new Promise(res => {
-        document.addEventListener('DOMContentLoaded', function () {
-            // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-            // // The Firebase SDK is initialized and available here!
-            //
-            // firebase.auth().onAuthStateChanged(user => { });
-            // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
-            // firebase.firestore().doc('/foo/bar').get().then(() => { });
-            // firebase.functions().httpsCallable('yourFunction')().then(() => { });
-            // firebase.messaging().requestPermission().then(() => { });
-            // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
-            // firebase.analytics(); // call to activate
-            // firebase.analytics().logEvent('tutorial_completed');
-            // firebase.performance(); // call to activate
-            //
-            // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-
-            try {
-                let app = firebaseAppInit();
-                let features = [
-                    'auth',
-                    'database',
-                    'firestore',
-                    'functions',
-                    'messaging',
-                    'storage',
-                    'analytics',
-                    'remoteConfig',
-                    'performance',
-                ].filter(feature => typeof app[feature] === 'function');
-                console.log(`Firebase SDK loaded with ${features.join(', ')}`);
-            } catch (e) {
-                console.error(e);
-                console.log('Error loading the Firebase SDK, check the console.');
-            }
-            finally {
-                res(firebase);
-            }
+    try {
+        const app = firebase.initializeApp({
+            apiKey: API_KEY,
+            authDomain: "fir-bb3d4.firebaseapp.com",
+            projectId: "fir-bb3d4",
+            storageBucket: "fir-bb3d4.appspot.com",
+            messagingSenderId: "762083454108",
+            appId: "1:762083454108:web:793ce977584f6c11115159",
+            measurementId: "G-TGB5CNB1FK"
         });
-    })
+    
+        let features = [
+            'auth',
+            'database',
+            'firestore',
+            'functions',
+            'messaging',
+            'storage',
+            'analytics',
+            'remoteConfig',
+            'performance',
+        ].filter(feature => typeof app[feature] === 'function');
+        console.log(`Firebase SDK loaded with ${features.join(', ')}`);
+        
+        isValid = true;
+    } catch (e) {
+        console.error(e);
+        console.log('Error loading the Firebase SDK, check the console.');
+
+        isValid = false;
+    }
+    finally {
+        return isValid;
+    }
 }
 
-(async function main() {
-    const firebase = await firebaseSdkCheck();
+function removeAuth() {
+    document.getElementById("youtube-oauth").remove();
+}
+
+async function authFlow() {
+    const { firebase } = window;
 
     const provider = new firebase.auth.GoogleAuthProvider();
 
     firebase.auth().languageCode = 'ko';
+
+    provider.addScope("https://www.googleapis.com/auth/youtube.readonly");
 
     provider.setCustomParameters({
         'login_hint': 'user@example.com'
@@ -68,27 +61,19 @@ async function firebaseSdkCheck() {
         firebase.auth().signInWithRedirect(provider);
     })
 
-    firebase.auth()
+    const returnData = {
+        token: undefined,
+        profile: undefined
+    };
+
+    await firebase.auth()
         .getRedirectResult()
         .then((result) => {
             if (result.credential) {
                 /** @type {firebase.auth.OAuthCredential} */
-                var credential = result.credential;
-
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                var token = credential.accessToken;
-                
-
-                document.getElementById("youtube-oauth").remove();
+                const credential = result.credential;
+                returnData.token = credential.accessToken;
             }
-            // The signed-in user info.
-            var user = result.user;
-            // IdP data available in result.additionalUserInfo.profile.
-            // ...
-
-            console.log(token)
-            console.log("user", result.additionalUserInfo.profile);
-
         }).catch((error) => {
             // Handle Errors here.
             var errorCode = error.code;
@@ -97,6 +82,27 @@ async function firebaseSdkCheck() {
             var email = error.email;
             // The firebase.auth.AuthCredential type that was used.
             var credential = error.credential;
-            // ...
+
+            alert(`${errorCode}: ${errorMessage}\nyou typed: ${email}`);
         });
-})();
+
+        return returnData;
+}
+
+function checkStreamsIsLive() {
+
+}
+
+async function main() {
+    const fbValid = firebaseAppInit();
+
+    if (!fbValid) return;
+
+    const data = await authFlow();
+
+    if (data.token === undefined) return;
+
+    removeAuth();
+}
+
+document.addEventListener('DOMContentLoaded', main);
